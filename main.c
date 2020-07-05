de1601d4a6540eefb1fb17a089777e620ecab4d5
MoraPresence <od34156la@mail.ru>
2020-07-05 16:02:20 +0300

    version 0.0.1

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+unsigned getBinaryLenght(unsigned num){
+    unsigned count = 1;
+    while(num > 1){
+        num /= 2;
+        ++count;
+    }
+    return count;
+}
+unsigned getRemainder(unsigned int dividend, unsigned int divisor) {
+    unsigned tmpDivisor = 0;
+    while (divisor < dividend) {
+        tmpDivisor = divisor;
+        tmpDivisor <<= getBinaryLenght(dividend) - getBinaryLenght(tmpDivisor);
+        dividend ^= tmpDivisor;
+    }
+    return dividend;
+}
+
+unsigned summ(unsigned a, unsigned b, unsigned p) {
+    return  getRemainder(a^b, p);
+}
+
+unsigned mul(unsigned a, unsigned b, unsigned p) {
+    unsigned x = 0;
+    while (b != 0) {
+        if ((b & 0x1) == 0x1)
+            x ^= a;
+        b >>= 1;
+        a <<= 1;
+    }
+    return getRemainder(x, p);
+}
