4608b5592bc7319abf837603c9de196d39e742ed
MoraPresence <od34156la@mail.ru>
2020-07-07 16:45:12 +0300

    version 0.0.3

--- main.c
+++ main.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdint.h>
 
-unsigned getBinaryLenght(unsigned num){
-    unsigned count = 1;
-    while(num > 1){
-        num /= 2;
-        ++count;
-    }
-    return count;
-}
-unsigned getRemainder(unsigned int dividend, unsigned int divisor) {
+//static const int lut[32] = {0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+//                            8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31};
+//
+//static unsigned int number_of_bits(unsigned int v) {
+//    v |= v >> 1;
+//    v |= v >> 2;
+//    v |= v >> 4;
+//    v |= v >> 8;
+//    v |= v >> 16;
+//
+//    return lut[(unsigned int) (v * 0x07C4ACDDU) >> 27] + 1;
+//} интересный способ
+
+unsigned getRemainder(unsigned dividend, unsigned divisor) {
     unsigned tmpDivisor = 0;
     while (dividend >= divisor) {
         tmpDivisor = divisor;
-        tmpDivisor <<= getBinaryLenght(dividend) - getBinaryLenght(tmpDivisor);
+        while (((dividend ^ tmpDivisor) >= dividend || ((dividend ^ tmpDivisor) >= tmpDivisor)))
+            tmpDivisor <<= 1;
         dividend ^= tmpDivisor;
     }
     return dividend;
 }
 
-unsigned summ(unsigned a, unsigned b, unsigned p) {
-    return  getRemainder(a^b, p);
+unsigned summ(unsigned a, unsigned b) {
+    return a ^ b;
 }
 
 unsigned mul(unsigned a, unsigned b, unsigned p) {
@@ -35,4 +37,11 @@ unsigned mul(unsigned a, unsigned b, unsigned p) {
         a <<= 1;
     }
     return getRemainder(x, p);
+}
+
+int main() {
+
+    printf("%d\n", mul(83, 9, 0b111000011));
+    printf("%d", summ(83, 9));
+    return 0;
 }
