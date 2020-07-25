f6bb1a6856f7e9d9128df76f649e040684ced7f7
MoraPresence <54858588+MoraPresence@users.noreply.github.com>
2020-07-25 06:59:57 -0700

    add reverse L & R & S

--- main.c
+++ main.c
@@ -33,6 +33,29 @@ unsigned pi[256] = {252, 238, 221, 17, 207, 110, 49, 22, 251, 196, 250, 218, 35,
                     97, 32, 113, 103, 164, 45, 43, 9, 91, 203, 155, 37, 208, 190, 229, 108, 82, 89, 166,
                     116, 210, 230, 244, 180, 192, 209, 102, 175, 194, 57, 75, 99, 182};
 
+unsigned reversePi[256] = { 0xa5, 0x2d, 0x32, 0x8f, 0x0e, 0x30, 0x38, 0xc0, 0x54, 0xe6, 0x9e, 0x39,
+                             0x55, 0x7e, 0x52, 0x91, 0x64, 0x03, 0x57, 0x5a, 0x1c, 0x60, 0x07, 0x18,
+                             0x21, 0x72, 0xa8, 0xd1, 0x29, 0xc6, 0xa4, 0x3f, 0xe0, 0x27, 0x8d, 0x0c,
+                             0x82, 0xea, 0xae, 0xb4, 0x9a, 0x63, 0x49, 0xe5, 0x42, 0xe4, 0x15, 0xb7,
+                             0xc8, 0x06, 0x70, 0x9d, 0x41, 0x75, 0x19, 0xc9, 0xaa, 0xfc, 0x4d, 0xbf,
+                             0x2a, 0x73, 0x84, 0xd5, 0xc3, 0xaf, 0x2b, 0x86, 0xa7, 0xb1, 0xb2, 0x5b,
+                             0x46, 0xd3, 0x9f, 0xfd, 0xd4, 0x0f, 0x9c, 0x2f, 0x9b, 0x43, 0xef, 0xd9,
+                             0x79, 0xb6, 0x53, 0x7f, 0xc1, 0xf0, 0x23, 0xe7, 0x25, 0x5e, 0xb5, 0x1e,
+                             0xa2, 0xdf, 0xa6, 0xfe, 0xac, 0x22, 0xf9, 0xe2, 0x4a, 0xbc, 0x35, 0xca,
+                             0xee, 0x78, 0x05, 0x6b, 0x51, 0xe1, 0x59, 0xa3, 0xf2, 0x71, 0x56, 0x11,
+                             0x6a, 0x89, 0x94, 0x65, 0x8c, 0xbb, 0x77, 0x3c, 0x7b, 0x28, 0xab, 0xd2,
+                             0x31, 0xde, 0xc4, 0x5f, 0xcc, 0xcf, 0x76, 0x2c, 0xb8, 0xd8, 0x2e, 0x36,
+                             0xdb, 0x69, 0xb3, 0x14, 0x95, 0xbe, 0x62, 0xa1, 0x3b, 0x16, 0x66, 0xe9,
+                             0x5c, 0x6c, 0x6d, 0xad, 0x37, 0x61, 0x4b, 0xb9, 0xe3, 0xba, 0xf1, 0xa0,
+                             0x85, 0x83, 0xda, 0x47, 0xc5, 0xb0, 0x33, 0xfa, 0x96, 0x6f, 0x6e, 0xc2,
+                             0xf6, 0x50, 0xff, 0x5d, 0xa9, 0x8e, 0x17, 0x1b, 0x97, 0x7d, 0xec, 0x58,
+                             0xf7, 0x1f, 0xfb, 0x7c, 0x09, 0x0d, 0x7a, 0x67, 0x45, 0x87, 0xdc, 0xe8,
+                             0x4f, 0x1d, 0x4e, 0x04, 0xeb, 0xf8, 0xf3, 0x3e, 0x3d, 0xbd, 0x8a, 0x88,
+                             0xdd, 0xcd, 0x0b, 0x13, 0x98, 0x02, 0x93, 0x80, 0x90, 0xd0, 0x24, 0x34,
+                             0xcb, 0xed, 0xf4, 0xce, 0x99, 0x10, 0x44, 0x40, 0x92, 0x3a, 0x01, 0x26,
+                             0x12, 0x1a, 0x48, 0x68, 0xf5, 0x81, 0x8b, 0xc7, 0xd6, 0x20, 0x0a, 0x08,
+                             0x00, 0x4c, 0xd7, 0x74};
+
 unsigned tableForL[16] = {148, 32, 133, 16, 194, 192, 1, 251, 1, 192, 194, 16, 133, 32, 148, 1};
 
 union uint128 {
@@ -51,7 +74,7 @@ uint128 R(uint128 a);
 
 uint128 L(uint128 a);
 
-uint8_t getRemainder(uint16_t  dividend, uint16_t divisor);
+uint8_t getRemainder(uint16_t dividend, uint16_t divisor);
 
 unsigned summ(unsigned a, unsigned b);
 
@@ -78,6 +101,15 @@ uint128 S(uint128 a) {
     return c;
 }
 
+uint128 reverseS(uint128 a) {
+    uint128 c;
+    int i;
+    for (i = 0; i < 16; ++i) {
+        c.b[i] = reversePi[a.b[i]];
+    }
+    return c;
+}
+
 uint128 R(uint128 a) {
     uint128 c;
     unsigned sum = 0;
@@ -88,6 +120,21 @@ uint128 R(uint128 a) {
     c.b[15] = sum;
     for (i = 1; i < 16; ++i) {
         c.b[15 - i] = a.b[16 - i];
+    }
+    return c;
+}
+
+uint128 reverseR(uint128 a) {
+    uint128 c;
+    unsigned sum = 0;
+    int i;
+    for (i = 0; i < 15; ++i) {
+        sum ^= fastMult(a.b[14 - i], tableForL[i]);
+    }
+    sum ^= fastMult(a.b[15], tableForL[15]);
+    c.b[0] = sum;
+    for (i = 0; i < 15; ++i) {
+        c.b[15 - i] = a.b[14 - i];
     }
     return c;
 }
@@ -113,11 +160,35 @@ uint128 L(uint128 a) {
     return c;
 }
 
+uint128 reverseL(uint128 a) {
+    uint128 c;
+    uint8_t tmp[32];
+    uint128 tmpB;
+    int i = 0;
+    int j = 0;
+    memset(tmp, 0, sizeof(uint8_t) * 32);
+   // memcpy(tmp, a.b, 16);
+    for(i = 0; i < 16; ++i){
+        tmp[16 + i] = a.b[i];
+    }
+    for (i = 0; i < 16; ++i) {
+        for (j = 0; j < 16; ++j) {
+            tmpB.b[15 - j] = tmp[31 - (j + i)];
+        }
+        tmp[15 - i] = reverseR(tmpB).b[0];
+    }
+
+    for (i = 0; i < 16; ++i) {
+        c.b[i] = tmp[i];
+    }
+    return c;
+}
+
 uint8_t getRemainder(uint16_t dividend, uint16_t divisor) {
     uint16_t mask = 0x8000;
     divisor <<= 7;
     while (dividend & 0xff00) {
-       // printf("a=0x%x, d=0x%x, m=0x%x (%x)\n", dividend, divisor, mask, (dividend & 0xff00));
+        // printf("a=0x%x, d=0x%x, m=0x%x (%x)\n", dividend, divisor, mask, (dividend & 0xff00));
         if (dividend & mask)
             dividend ^= divisor;
         mask >>= 1;
@@ -175,13 +246,23 @@ int main() {
         printf("%016llx", c.qw[1 - i]); //b66cd8887d38e8d77765aeea0c9a7efc
     }
     printf("\n");
+    c = reverseS(c);
+    for (i = 0; i < 2; ++i) {
+        printf("%016llx", c.qw[1 - i]);
+    }
+    printf("\n");
     uint128 d;
 
     d.qw[0] = 0x0000000000000000;
-    d.qw[1] = 0x64a5940000000000;
+    d.qw[1] = 0xa594000000000000;
     c = R(d);
     for (i = 0; i < 16; ++i) {
         printf("%02x", c.b[15 - i]); //0d64a594000000000000000000000000
+    }
+    printf("\n");
+    c = reverseR(c);
+    for (i = 0; i < 16; ++i) {
+        printf("%02x", c.b[15 - i]);
     }
     printf("\n");
     uint128 a1;
@@ -190,7 +271,13 @@ int main() {
     a1.qw[1] = 0x0e93691a0cfc6040;
     c = L(a1);
     for (i = 0; i < 16; ++i) {
-        printf("%02x", c.b[15 - i]); //e6a8094fee0aa204fd97bcb0b44b8580
+        printf("%02x", c.b[15 - i]);
     }
+    printf("\n");
+    c = reverseL(c);
+    for (i = 0; i < 16; ++i) {
+        printf("%02x", c.b[15 - i]);
+    }
+    printf("\n");
     return 0;
 }
